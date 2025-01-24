3ac313d9a31edeceb29ff1ca457636590fdf83bc
Partheev-v <vpartheevjoker@gmail.com>
2025-01-24 12:52:57 +0530

    new

---
+++ new.js
@@ -0,0 +1,13 @@
+let user={
+    name:"Patel",
+    age:30
+    "is Singer":true,
+}
+console.log(user);
+for(let key in user){
+    console.log(key+":"+user[key]);
+}
+const array=[1,2,3,4,5]
+for(let value of array){
+    console.log(value);
+}
